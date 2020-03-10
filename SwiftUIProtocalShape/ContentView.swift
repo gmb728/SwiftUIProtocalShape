d338bdff7de9cb4f97dd734e7e97ec722f60780a
Sophia <gmb728@gmail.com>
2020-03-10 15:22:13 +0800

    init

--- SwiftUIProtocalShape/ContentView.swift
+++ SwiftUIProtocalShape/ContentView.swift
@@ -8,14 +8,67 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+        Image("tree")
+        .resizable()
+        .scaledToFill()
+            VStack{
+                Leaf()
+                .fill(Color.green)
+                .frame(width:20, height: 25)
+                .rotationEffect(.degrees(30))
+                .offset(x:-40, y:-90)
+                Leaf()
+                .fill(Color.green)
+                .frame(width:20, height: 25)
+                .rotationEffect(.degrees(30))
+                .offset(x:-70, y:-90)
+                Leaf()
+                .fill(Color.green)
+                .frame(width:20, height: 25)
+                .rotationEffect(.degrees(10))
+                .offset(x:-90, y:-90)
+                Leaf()
+                .fill(Color.green)
+                .frame(width:20, height: 25)
+                .rotationEffect(.degrees(-40))
+                .offset(x:-120, y:-70) //checked
+                 
+                    
+            }
+        }
     }
+
 }
 
+
+struct Leaf: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { (path) in //由參數rect控制大小，rect是Leaf的框框大小
+            path.move(to: CGPoint(x:rect.width, y:0))
+            path.addQuadCurve(to: CGPoint(x:0, y:rect.height), control: CGPoint(x: rect.width/5, y: rect.height/5))
+            path.addQuadCurve(to: CGPoint(x:rect.width, y:0), control: CGPoint(x:rect.width * 17/20, y:rect.height * 13/15))
+            path.closeSubpath()
+        }
+     
+    }
+     
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExtractedView: View {
+    var body: some View {
+        Leaf()
+            .fill(Color.green)
+            .frame(width:20, height: 25)
+            .rotationEffect(.degrees(40))
+            .offset(x:-40, y:-90)
     }
 }
